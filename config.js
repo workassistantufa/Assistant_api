8db32a599caa8571b128080db2ac2792eb688ff8
workassistantufa <workassistantufa@yandex.ru>
2021-05-27 21:48:07 +0500

    change config.js

--- config.js
+++ config.js
@@ -16,10 +16,6 @@ module.exports.dbConfig ={
 };
 */
 
-//npx prisma migrate - помощь
-//npx prisma migrate reset - всё сбросить
-//npx prisma migrate dev - применить изменения
-//npx prisma migrate deploy - работа в продакшене
 
 //sendMail Настройки отправки почты, от имени кого отправлять
 module.exports.sendMail_host = 'smtp.yandex.ru';
@@ -35,33 +31,3 @@ module.exports.bot_token = '1428565883:AAEaSPndJe4RC3-dF-YQ-lsYTfX_1I7IYQ8';
 module.exports.kdinar_chat_id = '400738545';
 //Группа Башпринт Ассистент
 module.exports.group_chat_id = '-431221987';
-
-//Мэппинг docID таблиц
-module.exports.docID = {
-    'Пользователи': 1,
-    'Группы товаров': 2,
-    'Товары': 3,
-    'Клиенты': 4,
-    'Корзина': 5,
-    'Настройки': 6,
-    'Группы услуг': 7,
-    'Услуги': 8,
-    'Заявки': 9,
-    'Информирование': 10,
-    'Информирование, группы рассылки': 11,
-    'Группы': 12,
-    'Данные групп': 13,
-    'Номенклатура': 14,
-    'Группы номенклатуры': 15,
-    'Файлы': 16
-};
-
-//Мэппинг способов Информирования //Тип информирования
-module.exports.informType = {
-    0: 'Email',
-    1: 'Telegram',
-    2: 'SMS',
-    3: 'VK',
-    4: 'Instagram',
-    5: 'Viber'
-};
